dfb8e254813822b87d2a4f38a35406610f52a780
dwane richards <dwanerich@gmail.com>
2019-12-21 22:42:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |key, value|
       puts "#{key.capitalize}:"
       value.each do |holiday_key, val|
-        puts "  #{holiday_key.capitalize}: " + val.join(", ")
+        puts "  #{holiday_key.delete("_").capitalize}: " + val.join(", ")
       end
     end
     
