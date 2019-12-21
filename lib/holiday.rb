3bce280b5c520cc13df261023f25d9c0be9bddc1
dwane richards <dwanerich@gmail.com>
2019-12-21 23:01:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_holidays_with_bbq(holiday_hash)
     new_array = []
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, stuff|
-        new_array << if stuff.include?("BBQ")
+        new_array << holiday if stuff.include?("BBQ")
       end
     end
   end
